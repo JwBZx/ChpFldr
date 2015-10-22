0c811211d26dacabe523dd1ce04b6cd20e452fb3
JwBZx <Jwbmw3@gmail.com>
2015-10-21 22:38:57 -0400

    ChpFldr (JavaScript File)

---
+++ ChpFldr/ChpFldr.js
@@ -0,0 +1,37 @@
+console.log("Welcome to ChpFldr!");
+console.log("This is YOUR MegaMan Battle Network Chip Folder and guide!");
+console.log("By @JwBZx");
+// I hope that you like this Project!
+var editYN = false;
+var viewYN = false;
+var infoYN = false;
+var aboutYN = false;
+// Introduction:
+console.log("Hello! My name is MegaMan, and I am your Personal NetNavi!");
+console.log("What do you want to do? You can:");
+console.log("EDIT your Chip Folder [Type 'EDIT']");
+console.log("VIEW your Chip Folder [Type 'VIEW']");
+console.log("(get) INFO on your Battlechips");
+console.log("(find out) ABOUT the people who made this ChpFldr Program!");
+// The Starting Block (Prompts):
+prompt("So what do you want to do? [Type one of the four choices ABOVE]");
+if(prompt = "edit".toUppercase) {
+	var editYN = true;
+} else if(prompt = "view".toUppercase) {
+	var viewYN = true;
+} else if(prompt = "info".toUppercase) {
+	var infoYN = true;
+} else if(prompt = "about".toUppercase) {
+	var aboutYN = true;
+} else {
+	console.log("Oops! It didn't work...Try typing in either 'EDIT', 'VIEW', 'INFO', or 'ABOUT'!");
+}
+// Edit Your Folder
+
+// View Your Folder
+
+// Get Information About Stuff
+
+// Find Out About Us!
+
+// The Finish Line (End):
